08386c76a537d20626cd397b7c527aab72e86bd6
Bryandlc12 <160089099+Bryandlc12@users.noreply.github.com>
2024-10-19 18:08:13 -0600

    examen.java
    programa en java

---
+++ examen.java
@@ -0,0 +1,11 @@
+public class examen01 {
+
+    public static void main(string args[]) {
+
+        string puesto;
+        double salario = 0;
+        int antiguedad = 0;
+        double aumento, salatotal;
+      
+    }
+}
